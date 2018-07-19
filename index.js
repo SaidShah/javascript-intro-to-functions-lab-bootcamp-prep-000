06e616f873b48acabde5aa298c99c730a42b8dd8
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-07-19 19:10:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,3 +19,23 @@ function sayHiToGrandma(string){
     return "I can't hear you"
   }
 }
+
+function sayHiToGrandma(string){
+  if(string===string.toLowerCase()){
+    return "I can't hear you"
+  }
+  if(string ===string.toUpperCase()){
+    return "YES INDEED!"
+  }
+  if(string==="I love you, Grandma."){
+    return "I love you, too."
+  }
+}
+
+
+
+
+
+
+
+
