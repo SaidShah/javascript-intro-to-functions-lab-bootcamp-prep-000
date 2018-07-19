7a190eea6bc5779ff4208417941a7dd84146f18c
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-07-19 19:13:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,15 +14,10 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string){
-  if(string===string.toLowerCase()){
-    return "I can't hear you!"
-  }
-}
 
 function sayHiToGrandma(string){
   if(string===string.toLowerCase()){
-    return "I can't hear you"
+    return "I can't hear you!"
   }
   if(string ===string.toUpperCase()){
     return "YES INDEED!"
